3c3f7860bb1a68314f6779ee5ff69e8cbc4caf44
elmarmisirzada <emisirzada18219@ada.edu.az>
2025-12-27 22:54:22 +0400

    Add files via upload

---
+++ src/components/MeetAlumni.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './MeetAlumni.css'
+
+const MeetAlumni: React.FC = () => {
+  return (
+    <div className="total-meet">
+        <div className="container">
+            <div className="top-alumni">
+                <h1>Meet Our Alumni</h1>
+                <button>
+                    Learn More
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MeetAlumni
