494d016e926ae7345f251914ffc873b34e5c612c
elmarmisirzada <emisirzada18219@ada.edu.az>
2025-12-27 22:50:37 +0400

    Add files via upload

---
+++ src/components/DiscoverADA.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './DiscoverADA.css'
+
+const DiscoverADA: React.FC = () => {
+    return (
+        <div className="discover-fullparts">
+            <div className='discover-little-parts container'>
+                <div className="discover-left">
+                    <h1>
+                        Discover ADA University Campus</h1>
+                    <p>Curious to explore our state-of-the-art campus?
+
+                    </p>
+                </div>
+
+                <div className="discover-right">
+                    <h1>Join Events</h1>
+                    <button>
+                        Check Upcoming Events
+                    </button>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default DiscoverADA
